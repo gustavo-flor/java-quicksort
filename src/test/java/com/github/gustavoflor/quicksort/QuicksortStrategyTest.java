71c2317650a3a1595bbfe6f745ef30966eb8e307
gustavo-flor <gustavo.flor@picpay.com>
2024-02-13 17:25:36 -0300

    :tada: Hello Quicksort

--- src/test/java/com/github/gustavoflor/quicksort/QuicksortStrategyTest.java
+++ src/test/java/com/github/gustavoflor/quicksort/QuicksortStrategyTest.java
@@ -11,7 +11,7 @@ import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-class QuicksortTest {
+class QuicksortStrategyTest {
     @ParameterizedTest
     @MethodSource("getPartitionStrategies")
     @DisplayName("""
